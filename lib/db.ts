60ff710c59e3b0e5135a4b05c3193ae87072135b
karl <enriquezkharl14@gmail.com>
2026-05-29 12:43:20 +0800

    [Feat] Better auth drizzle adapter Neon connection

---
+++ lib/db.ts
@@ -0,0 +1,7 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const sql = neon(connectionString);
+
+export const db = drizzle({ client: sql });
