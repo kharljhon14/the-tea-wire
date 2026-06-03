57da8c31a03c2852baaebcd2a91869d22aa6527a
karl <enriquezkharl14@gmail.com>
2026-06-03 21:22:40 +0800

    [Chore] re apply shadcn styles and form components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/sonner';
 
-const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,13 +30,12 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn(
-        'h-full',
-        'antialiased',
-        geistSans.variable,
-        geistMono.variable,
-        'font-sans',
-        geist.variable
-      )}
+              'h-full',
+              'antialiased',
+              geistSans.variable,
+              geistMono.variable,
+              'font-sans'
+            , "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
         {children}
