7dc7763ea22b615e9a5c22ed2015ad2b8ea7f7ff
karl <enriquezkharl14@gmail.com>
2026-06-08 02:11:08 +0800

    [Feat] Update post

--- features/posts/server/routers.ts
+++ features/posts/server/routers.ts
@@ -1,6 +1,7 @@
 import { db } from '@/lib/db';
 import { posts } from '@/schema/post-schema';
 import { createTRPCRouter, proctedProcedure } from '@/trpc/init';
+import { and, eq } from 'drizzle-orm';
 import z from 'zod';
 
 export const postsRouter = createTRPCRouter({
@@ -15,5 +16,23 @@ export const postsRouter = createTRPCRouter({
         text: input.text,
         userId: ctx.userId
       });
+    }),
+
+  update: proctedProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, 'id is required'),
+        text: z.string().min(1, 'text is required')
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, text } = input;
+
+      return db
+        .update(posts)
+        .set({
+          text
+        })
+        .where(and(eq(posts.userId, ctx.userId), eq(posts.id, id)));
     })
 });
